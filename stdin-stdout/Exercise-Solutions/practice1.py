c7b7f26a4fad72c16355a3ac8b1b210121e50398
Aiproland <uplimitopenai@gmail.com>
2024-08-04 14:33:26 +0330

    Add files via upload

---
+++ stdin-stdout/Exercise-Solutions/practice1.py
@@ -0,0 +1,9 @@
+import sys
+
+firstname = sys.argv[1]
+lastname = sys.argv[2]
+
+sys.stdout.write(firstname + " " + lastname)
+
+# in python commandline type and execute:  "python practice1.py [your firstname] [your lastname]"
+        
